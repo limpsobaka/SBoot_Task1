3f86214cf22a20301c003d28b43aa318b692b6c7
Limpsobaka <limpsobaka@yandex.ru>
2023-02-19 22:16:32 +0300

    Initial commit

---
+++ src/main/java/ru/netology/sboot_task1/SBootTask1Application.java
@@ -0,0 +1,14 @@
+package ru.netology.sboot_task1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SBootTask1Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SBootTask1Application.class, args);
+
+  }
+
+}
---
+++ src/main/java/ru/netology/sboot_task1/profile/SystemProfile.java
@@ -0,0 +1,5 @@
+package ru.netology.sboot_task1.profile;
+
+public interface SystemProfile {
+  String getProfile();
+}
